8100e45f8f07ae58c09747f5c22d2333556a606d
sravanprema <32254602+sravanprema@users.noreply.github.com>
2018-11-23 14:09:59 +0530

    Add files via upload

---
+++ bubblesort.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int a[100],n,temp;
+printf("enter the size:\n");
+scanf("%d",&n);
+printf("enter the elements");
+for(int i=0;i<n;i++)
+scanf("%d",&a[i]);
+for(int i=0;i<n;i++)
+for(int j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+printf("SORTED ELEMENTS:");
+for(int i=0;i<n;i++)
+printf("%d\t",a[i]);
+}
